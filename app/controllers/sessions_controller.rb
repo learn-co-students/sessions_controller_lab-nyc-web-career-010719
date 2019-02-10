6d8111028a3888ee245b71b833e2acbf1a13137b
jbharwood <josephblakeharwood@gmail.com>
2019-02-10 15:36:47 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :name
-    redirect_to controller: 'application', action: 'hello'
+    redirect_to controller: 'sessions', action: 'new'
   end
 
 end
